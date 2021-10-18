2578876da851610557196de7b4fa80fa4d6de369
thomasdevolder <thomas.devolder@student.howest.be>
2021-10-18 17:38:32 +0200

    test

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b10f34bb61b1c9884c087878691b38e5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2320358d-3986-479e-a9b9-703ae69866bf'
+};
